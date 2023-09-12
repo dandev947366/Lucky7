61f3005ef84084969b17260469009cb7d310ea44
Dan Le <dan.dev947366@gmail.com>
2023-09-12 15:24:42 +0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,58 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int bet = 5; 
+        boolean reset = true;
+
+        while (bet > 0 && reset) {
+        
+            int num1 = random();
+            int num2 = random();
+            int num3 = random();
+
+            System.out.println("Random numbers: " + num1 + ", " + num2 + ", " + num3);
+
+            int win = checkWin(num1, num2, num3);
+            if (win > 0) {
+                bet += win;
+                System.out.println("You won $" + win);
+            } else {
+                bet -= 1; 
+            
+            }
+
+            if (bet <= 0) {
+                System.out.println("No more money. Game over!!!!");
+                break;
+            }
+
+            System.out.print("You have " + bet + "Play again? (y/n): ");
+            String choice = scanner.next();
+           reset = choice.equalsIgnoreCase("yes");
+        }
+
+      System.out.println("Thank you.");
+        scanner.close();
+    }
+
+    public static int random() {
+        Random rand = new Random();
+        return rand.nextInt(10) + 1;
+    }
+
+    
+    public static int checkWin(int num1, int num2, int num3) {
+        if (num1 == 7 && num2 == 7 && num3 == 7) {
+            return 10; 
+        } else if (num1 == 7 || num2 == 7 || num3 == 7) {
+            return 3; 
+        } else if ((num1 == 7 && num2 == 7) || (num1 == 7 && num3 == 7) || (num2 == 7 && num3 == 7)) {
+            return 5; 
+        }
+        return 0; 
+    }
+}
